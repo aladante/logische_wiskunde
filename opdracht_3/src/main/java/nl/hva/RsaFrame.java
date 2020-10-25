c7ae5351cf20299812ae6f373d0bab72deeb6516
Bas van der Linden <bvanderlinden1@gmail.com>
2020-10-25 13:42:27 +0100

    Adding scrollable lobal + other layout improvements

--- opdracht_3/src/main/java/nl/hva/RsaFrame.java
+++ opdracht_3/src/main/java/nl/hva/RsaFrame.java
@@ -13,12 +13,12 @@ public class RsaFrame {
     private static Rsa rsa = new Rsa();
 
     private static int width = 750;
-    private static int height = 675;
+    private static int height = 575;
     private static Border border = BorderFactory.createCompoundBorder(
             new EtchedBorder(),
             new EmptyBorder(10, 10, 10, 10)
     );
-    private static Dimension spacing = new Dimension(0, 10);
+    private static Dimension spacing = new Dimension(0, 20);
 
     public RsaFrame() {
 
@@ -44,92 +44,93 @@ public class RsaFrame {
 
     private static JPanel leftPanel() {
         JPanel panelLeft = genericPanel();
-        panelLeft.add(new JLabel("Encryption"));
-        panelLeft.add(Box.createRigidArea(spacing));
-        JPanel panelInputN = inputPanel("N = ");
-        JLabel q = new JLabel("p is  <value>");
-        JLabel p = new JLabel("q is  <value>");
-        JLabel time = new JLabel("time is ");
-        JLabel panelE = new JLabel("e is  <value>");
-        JLabel panelM = new JLabel("Message after encryption is: <c>");
-        panelLeft.add(panelInputN);
-        panelLeft.add(Box.createRigidArea(spacing));
-        panelLeft.add(Box.createRigidArea(spacing));
-        panelLeft.add(p);
-        panelLeft.add(Box.createRigidArea(new Dimension(0, 10)));
-        panelLeft.add(q);
-        panelLeft.add(Box.createRigidArea(new Dimension(0, 10)));
-        panelLeft.add(time);
 
-        panelLeft.add(new JButton(new AbstractAction("Step 1") {
+        addWithSpacingBelow(panelLeft, new JLabel("Encryption"));
+
+        JPanel panelInputN = inputPanel("N = ");
+        panelLeft.add(panelInputN);
+        addWithSpacingBelow(panelLeft, panelInputN);
+
+        JTextArea p = multilineLabel("p = <value>");
+        addWithSpacingBelow(panelLeft, addMultilineLabelToScrollPane(p));
+
+        JTextArea q = multilineLabel("q = <value>");
+        addWithSpacingBelow(panelLeft, addMultilineLabelToScrollPane(q));
+
+        JTextArea time = multilineLabel("time = <value>");
+        addWithSpacingBelow(panelLeft, addMultilineLabelToScrollPane(time));
+
+        addWithSpacingBelow(panelLeft, new JButton(new AbstractAction("Step 1") {
             @Override
             public void actionPerformed(ActionEvent e) {
                 // Perform Step 1
                 rsa.calculatePandQ(getTextFromInputPanel(panelInputN));
                 p.setText("p = " + rsa.getP());
                 q.setText("q = " + rsa.getQ());
-                time.setText("Tijd nodig = " + rsa.getCalculateTime());
+                time.setText("time = " + rsa.getCalculateTime());
             }
         }));
 
-        panelLeft.add(Box.createRigidArea(new Dimension(0, 10)));
-        panelLeft.add(new JButton(new AbstractAction("Step 2") {
+        JTextArea textE = multilineLabel("e = <value>");
+
+        addWithSpacingBelow(panelLeft, new JButton(new AbstractAction("Step 2") {
             @Override
             public void actionPerformed(ActionEvent e) {
                 // Perform Step 2
                 rsa.generateE();
                 System.out.println(rsa.getE() + "   is e");
-                panelE.setText("e is  " + rsa.getE());
+                textE.setText("e = " + rsa.getE());
             }
         }));
-        panelLeft.add(Box.createRigidArea(new Dimension(0, 10)));
-        panelLeft.add(panelE);
-        panelLeft.add(Box.createRigidArea(new Dimension(0, 10)));
+
+        addWithSpacingBelow(panelLeft, addMultilineLabelToScrollPane(textE));
+
         JPanel panelInputM = inputPanel("M = ");
-        panelLeft.add(panelInputM);
-        panelLeft.add(Box.createRigidArea(new Dimension(0, 10)));
-        panelLeft.add(new JButton(new AbstractAction("Step 3") {
+        addWithSpacingBelow(panelLeft, panelInputM);
+
+        JTextArea labelM = multilineLabel("message after encryption = <c>");
+
+        addWithSpacingBelow(panelLeft, new JButton(new AbstractAction("Step 3") {
             @Override
             public void actionPerformed(ActionEvent e) {
                 // Perform Step 3
                 String message = getTextFromInputPanel(panelInputM);
                 String encryptedMessage = rsa.encryptMessage(message);
                 System.out.println(encryptedMessage);
-                panelM.setText("M encrypted = " + encryptedMessage);
+                labelM.setText("M encrypted = " + encryptedMessage);
+                // labelM.setText("<html><p style=\"width:250px\">"+encryptedMessage+"</p></html>");
             }
         }));
-        panelLeft.add(Box.createRigidArea(new Dimension(0, 10)));
-        panelLeft.add(panelM);
-        panelLeft.add(Box.createRigidArea(new Dimension(0, 10)));
+
+        addWithSpacingBelow(panelLeft, addMultilineLabelToScrollPane(labelM));
+        panelLeft.add(Box.createRigidArea(new Dimension(0, 185)));
 
         return panelLeft;
     }
 
     private static JPanel rightPanel() {
         JPanel panelRight = genericPanel();
+
+        addWithSpacingBelow(panelRight, new JLabel("Decryption"));
+        
         JPanel panelInputE = inputPanel("E = ");
+        addWithSpacingBelow(panelRight, panelInputE);
+
         JPanel panelInputN = inputPanel("N = ");
+        addWithSpacingBelow(panelRight, panelInputN);
+
+        JTextArea valueD = multilineLabel("d = <value>");
+        addWithSpacingBelow(panelRight, addMultilineLabelToScrollPane(valueD));
+
         JPanel panelInputC = inputPanel("C = ");
-        JLabel valueD = new JLabel("d is <value>");
-        JLabel valueM = new JLabel("Message after decryption is: <m>");
+        addWithSpacingBelow(panelRight, panelInputC);
 
-        panelRight.add(new JLabel("Decryption"));
-        panelRight.add(Box.createRigidArea(spacing));
+        JTextArea valueM = multilineLabel("message after decryption = <m>");
+        addWithSpacingBelow(panelRight, addMultilineLabelToScrollPane(valueM));
 
-        panelRight.add(panelInputE);
-        panelRight.add(panelInputN);
-        panelRight.add(Box.createRigidArea(spacing));
-        panelRight.add(Box.createRigidArea(spacing));
-
-        panelRight.add(valueD);
-        panelRight.add(Box.createRigidArea(spacing));
-        panelRight.add(panelInputC);
-        panelRight.add(Box.createRigidArea(spacing));
-        panelRight.add(Box.createRigidArea(spacing));
-        panelRight.add(valueM);
         // this step takes input N and E to find D If
         // if p and q are to big this will take a long time
-        panelRight.add(new JButton(new AbstractAction("Step 1") {
+        addWithSpacingBelow(panelRight, new JButton(new AbstractAction("Step 1") {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String ee = getTextFromInputPanel(panelInputE);
@@ -140,7 +141,7 @@ public class RsaFrame {
             }
         }));
 
-        panelRight.add(new JButton(new AbstractAction("Step 2") {
+        addWithSpacingBelow(panelRight, new JButton(new AbstractAction("Step 2") {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String mess = getTextFromInputPanel(panelInputC);
@@ -150,11 +151,33 @@ public class RsaFrame {
             }
         }));
 
-        panelRight.add(Box.createRigidArea(spacing));
+        panelRight.add(Box.createRigidArea(new Dimension(0, 275)));
 
         return panelRight;
     }
 
+    private static JTextArea multilineLabel(String text) {
+        JTextArea multilineLabel = new JTextArea();
+        multilineLabel.setText(text);
+        multilineLabel.setLineWrap(true);
+        multilineLabel.setEditable(false);
+        multilineLabel.setOpaque(false);
+        Font boldFont = new Font(multilineLabel.getFont().getName(), Font.BOLD, multilineLabel.getFont().getSize());
+        multilineLabel.setFont(boldFont);
+        return multilineLabel;
+    }
+
+    private static JScrollPane addMultilineLabelToScrollPane(JTextArea multilineLabel) {
+        JScrollPane scrollPane = new JScrollPane(multilineLabel);
+        // scrollPane.setBorder(new EmptyBorder(0, 0, 0, 0));
+        scrollPane.setAlignmentX(Component.LEFT_ALIGNMENT);
+        return scrollPane;
+    }
+
+    private static void addWithSpacingBelow(JPanel panel, JComponent component) {
+        panel.add(component);
+        panel.add(Box.createRigidArea(spacing));
+    }
 
     private static JPanel genericPanel() {
         JPanel panel = new JPanel();
@@ -167,23 +190,26 @@ public class RsaFrame {
 
     private static JPanel inputPanel(String labelText) {
         JPanel panelInput = new JPanel();
-        JTextArea textArea = new JTextArea();
-        textArea.setLineWrap(true);
+        JTextField textField = new JTextField();
+        // JTextArea textArea = new JTextArea(1, 1);
+        // textField.setLineWrap(true);
+        // textArea.getStyle().setAlignment(RsaFrame.Center);
         panelInput.setAlignmentX(Component.LEFT_ALIGNMENT);
         panelInput.setLayout(new BoxLayout(panelInput, BoxLayout.X_AXIS));
         panelInput.add(new JLabel(labelText));
-        panelInput.add(textArea);
+        panelInput.add(textField);
 
         return panelInput;
     }
 
     private static void setTextFromInputPanel(JPanel inputPanel, String message) {
-        JTextArea textField = (JTextArea) inputPanel.getComponents()[1];
+        JTextField textField = (JTextField) inputPanel.getComponents()[1];
         textField.setText(message);
     }
 
     private static String getTextFromInputPanel(JPanel inputPanel) {
-        JTextArea textField = (JTextArea) inputPanel.getComponents()[1];
+        JTextField textField = (JTextField) inputPanel.getComponents()[1];
+        System.out.println(textField.getText());
         return textField.getText();
     }
 }
